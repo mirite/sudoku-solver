84d48b5084904e4544c58aa86a75f2a51eceed62
Jesse Conner <me@jesseconner.ca>
2024-09-21 12:05:29 -0400

    Test cleanup

--- src/tests/possible.rs
+++ src/tests/possible.rs
@@ -6,6 +6,7 @@ mod tests {
         is_only_possible_placement,
     };
     use std::fs::read_to_string;
+    use std::path::Path;
 
     #[test]
     fn can_calculate_possible_values() {
@@ -61,32 +62,22 @@ mod tests {
     #[test]
     fn can_solve() {
         let data = read_to_string("test_grids/easyToSolve.txt").unwrap();
-        let solved_data = read_to_string("test_grids/easyToSolveSolved.txt").unwrap();
         let grid = read_grid(data).unwrap();
-        let solved_grid = read_grid(solved_data).unwrap();
         let result = fill_inferred(grid);
-        for r in 0..9 {
-            for c in 0..9 {
-                print!("{}", result[r][c].provided);
-                if c % 3 == 2 && c != 8 {
-                    print!("|");
-                    assert_eq!(result[r][c].provided, solved_grid[r][c].provided);
-                }
-            }
-            print!("\n");
-            if r % 3 == 2 && r != 8 {
-                println!("-----------")
-            }
-        }
+        compare_grids(result, "test_grids/easyToSolveSolved.txt")
     }
 
     #[test]
     fn can_solve_2() {
         let data = read_to_string("test_grids/hardToSolve.txt").unwrap();
-        let solved_data = read_to_string("test_grids/hardToSolveSolved.txt").unwrap();
         let grid = read_grid(data).unwrap();
-        let solved_grid = read_grid(solved_data).unwrap();
         let result = fill_inferred(grid);
+        compare_grids(result, "test_grids/hardToSolveSolved.txt");
+    }
+
+    fn compare_grids(result: [[Cell; 9]; 9], actual_path: &str) {
+        let solved_data = read_to_string(actual_path).unwrap();
+        let solved_grid = read_grid(solved_data).unwrap();
         for r in 0..9 {
             for c in 0..9 {
                 print!("{}", result[r][c].provided);
